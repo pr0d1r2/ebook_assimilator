8b99e2cf54bca6378ab8a53998531d031ee1e724
Marcin Nowicki <pr0d1r2@gmail.com>
2010-11-02 20:02:30 +0100

    Add additional e-books.

--- assimilate.rb
+++ assimilate.rb
@@ -8,6 +8,60 @@ ebooks = [
     :title    => "Time Management for Creative People",
     :category => "Productivity",
     :url      => "http://wishful.fileburst.com/creativetime.pdf"
+  },
+  {
+    :author   => "Chris Brogan",
+    :title    => "Using the Social Web to Find Work",
+    :category => "Freelance",
+    :url      => "http://www.chrisbrogan.com/img/socialwebforwork.pdf"
+  },
+  {
+    :author   => "Chris Brogan",
+    :title    => "Personal Branding",
+    :category => "Freelance",
+    :url      => "http://www.chrisbrogan.com/img/broganbranding.pdf"
+  },
+  {
+    :author   => "Seth Godin",
+    :title    => "What Matters Now",
+    :category => "Freelance",
+    :url      => "http://sethgodin.typepad.com/files/what-matters-now-1.pdf"
+  },
+  {
+    :author   => "Sharon Hurley Hall",
+    :title    => "Getting Started in Blogging",
+    :category => "Blogging",
+    :url      => "http://www.getpaidtowriteonline.com/Ebooks/GSIB%20Ebook.pdf"
+  },
+  {
+    :author   => "Dave Navarro",
+    :title    => "More Time Now: Time Management Made Simple Again",
+    :category => "Productivity",
+    :url      => "http://www.rockyourday.com/manifesto/MoreTimeNow.pdf"
+  },
+  {
+    :author   => "Robert Schifreen",
+    :title    => "The Web Book",
+    :category => "Web Development",
+    :url      => "http://www.the-web-book.com/index.php?a=xdl"
+  },
+  {
+    :author   => "Mike Smith",
+    :title    => "Guide to Guerilla Freelancing",
+    :category => "Freelance",
+    :url      => "http://www.guerrillafreelancing.com/download/1"
+  },
+  {
+    :author   => "Michael Stelzner",
+    :title    => "2010 Social Media Marketing Industry Report",
+    :category => "Social Media",
+    :url      => "http://marketingwhitepapers.s3.amazonaws.com/SocialMediaMarketingReport2010.pdf"
+  },
+  {
+    :author   => "Peter Shallard",
+    :title    => "Seek and Destroy",
+    :category => "Freelance",
+    :url      => "http://www.petershallard.com/downloads/SeekAndDestroy.pdf"
   }
 ]
 
